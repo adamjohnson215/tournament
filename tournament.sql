b02907572b7a57657373b17ac629a27514cb8e02
Adam Johnson <adamjohnson215@gmail.com>
2015-08-03 15:00:32 -0400

    Add drop command for ease in testing Match cases in matches table with foreign keys Reformat create view statements

--- tournament.sql
+++ tournament.sql
@@ -3,6 +3,9 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 
+-- Drop existing tournament database
+DROP DATABASE IF EXISTS tournament;
+
 -- Create the tournament database
 CREATE DATABASE tournament;
 
@@ -14,36 +17,39 @@ CREATE TABLE players(id serial primary key, name text);
 
 -- Create the table matches to log output of all matches
 CREATE TABLE matches (match_id serial primary key, 
-					  winner_id text, loser_id text);
+					  winner_id integer, loser_id integer);
 
 -- Create a view of wins for each player	 
 CREATE VIEW wins as
-	SELECT players.id, count (matches.*) as wins 
+	SELECT players.id, 
+		   count (matches.*) as wins 
 		 FROM players left join matches 
-			ON players.id = CAST(matches.winner_id as int)
+			ON players.id = matches.winner_id
 		 GROUP BY players.id;
 
 -- Create a view of matches played by each player
 CREATE VIEW matches_played as
-	SELECT players.id, count (matches.*) as matches 
+	SELECT players.id, 
+	       count (matches.*) as matches 
 	FROM players left join matches 
-		ON players.id = CAST(matches.winner_id as int)
-		OR players.id = CAST(matches.loser_id as int)
+		ON players.id = matches.winner_id
+		OR players.id = matches.loser_id
 	GROUP BY players.id;
 
 -- Create a view of matches played and wins for each player
 CREATE VIEW matches_and_wins as
-	SELECT matches_played.id, sum (wins.wins) as wins, 
-			sum (matches_played.matches) as matches
+	SELECT matches_played.id, 
+		   sum (wins.wins) as wins, 
+		   sum (matches_played.matches) as matches
 	FROM matches_played left join wins
 		ON matches_played.id = wins.id
 	GROUP BY matches_played.id;
 
--- Create a view for player standings
+-- Create a view for player standings ordering by wins
 CREATE VIEW standings as 
 	SELECT players.id, players.name, 
-		sum(matches_and_wins.wins) as wins, 
-		sum(matches_and_wins.matches) as matches
+		   sum(matches_and_wins.wins) as wins, 
+		   sum(matches_and_wins.matches) as matches
 	FROM players left join matches_and_wins
 		ON players.id = matches_and_wins.id
 	GROUP BY players.id
